0f378692a1d3ac85595ac2d91bab5ce4192a7f41
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-09-02 13:07:32 +0300

    [Add] driver and passenger activities stub and processing checkout them

--- app/src/main/java/com/example/taxiapp/ChoseModeActivity.java
+++ app/src/main/java/com/example/taxiapp/ChoseModeActivity.java
@@ -2,7 +2,9 @@ package com.example.taxiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChoseModeActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class ChoseModeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chose_mode);
+    }
+
+    public void goToPassengerSignIn(View view) {
+        startActivity(new Intent(ChoseModeActivity.this,
+                PassangerSignInActivity.class));
+    }
+
+    public void goToDriverSignIn(View view) {
+        startActivity(new Intent(ChoseModeActivity.this,
+                DriverSignInActivity.class));
     }
 }
---
+++ app/src/main/java/com/example/taxiapp/PassangerSignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.taxiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PassangerSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passanger_sign_in);
+    }
+}
