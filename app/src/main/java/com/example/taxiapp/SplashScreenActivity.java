6c0041118f8922ffb6cbf8b977ec6ecb824a4785
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-09-02 12:51:37 +0300

    [Add] splash activity

---
+++ app/src/main/java/com/example/taxiapp/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.taxiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+
+                }
+            }
+        };
+    }
+}
